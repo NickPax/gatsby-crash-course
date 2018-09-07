2a12f44e666d78c07dd3528f16c17a4923974baa
NickPax <paxford@wp.pl>
2018-09-07 22:27:18 +0200

    menu styling

---
+++ path---post-two-d3cb627b3f8b13137ccf 2.js
@@ -0,0 +1,2 @@
+webpackJsonp([4430129151547],{336:function(t,o){t.exports={data:{markdownRemark:{html:"<p>This is my second blog post in Gatsby.</p>",frontmatter:{path:"/post-two",title:"Second gatsby post",author:"Jan",date:"2018-04-10"}}},pathContext:{}}}});
+//# sourceMappingURL=path---post-two-d3cb627b3f8b13137ccf.js.map
